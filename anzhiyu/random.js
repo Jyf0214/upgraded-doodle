7409343a6629b16999b3e0a645deb1817b4caa44
Jyf0214 <Jyf0214@users.noreply.github.com>
2026-01-03 03:02:53 +0000

    Update gh-pages branch from 67568751c5eeffe8e398be44fd9ef495a3085006

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/30/Free-Domains/","2026/01/03/hello-world/","2026/01/03/web-dl-manager/"];function toRandomPost(){
+var posts=["2024/11/30/Free-Domains/","2026/01/03/web-dl-manager/","2026/01/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
